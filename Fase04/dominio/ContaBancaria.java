54a4be40bf63679130ebf9319cbdd993b703571d
Gi-S-Lobo <giovanna.lobo@soulasalle.com.br>
2025-03-27 16:02:46 -0300

    fase 04

--- Fase04/dominio/ContaBancaria.java
+++ Fase04/dominio/ContaBancaria.java
@@ -1,24 +1,45 @@
 package dominio;
 
 public class ContaBancaria {
+    private String titular;
+    private double saldo;
 
-	public ContaBancaria(String titular) {
-		// TODO Auto-generated constructor stub
-	}
+    public ContaBancaria(String titular) {
+        this.titular = titular;
+        this.saldo = 0.0;
+    }
 
-	public void depositar(double valorDeposito) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+            System.out.println("Depósito de R$" + valor + " realizado com sucesso.");
+        } else {
+            System.out.println("Valor de depósito inválido.");
+        }
+    }
 
-	public void sacar(double valorSaque) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void sacar(double valor) {
+        if (valor > 0) {
+            if (saldo >= valor) {
+                saldo -= valor;
+                System.out.println("Saque de R$" + valor + " realizado com sucesso.");
+            } else {
+                System.out.println("Saldo insuficiente para realizar o saque.");
+            }
+        } else {
+            System.out.println("Valor de saque inválido.");
+        }
+    }
 
-	public void exibirSaldo() {
-		// TODO Auto-generated method stub
-		
-	}
+    public void exibirSaldo() {
+        System.out.println("Saldo atual: R$" + saldo);
+    }
 
+    public String getTitular() {
+        return titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 }
